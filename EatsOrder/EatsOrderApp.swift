422ce837f437a8b747df8e65feec14c50a89f3ef
WaterBean <gkstnqls9@gmail.com>
2025-05-09 16:50:23 +0900

    Initial Commit

---
+++ EatsOrder/EatsOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatsOrderApp.swift
+//  EatsOrder
+//
+//  Created by 한수빈 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct EatsOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EatsOrderTests/EatsOrderTests.swift
@@ -0,0 +1,17 @@
+//
+//  EatsOrderTests.swift
+//  EatsOrderTests
+//
+//  Created by 한수빈 on 5/9/25.
+//
+
+import Testing
+@testable import EatsOrder
+
+struct EatsOrderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
